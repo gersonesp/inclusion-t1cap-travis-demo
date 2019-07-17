ab3fc23b4831673ae10956da211f0b2be435c6f7
Gerson Espinal <gerson.espinal@gmail.com>
2019-07-17 19:16:36 -0400

    heroku

--- hello.js
+++ hello.js
@@ -7,6 +7,6 @@ http
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello Travis!\n");
   })
-  .listen(port, "127.0.0.1");
+  .listen(port, "0.0.0.0");
 
-console.log(`Server running at ${port}`);
+console.log(`Server running at http://0.0.0.0${port}`);
